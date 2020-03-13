e165551fd1f776a7a29ef9469c5905cf669e47c6
Kate McGee <mcgeehee@gmail.com>
2020-03-13 13:27:27 -0600

    added second test

--- client/src/components/PlayerCard.test.js
+++ client/src/components/PlayerCard.test.js
@@ -5,7 +5,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 
 
 
-  test('renders main page', () => {
+  test('renders heading from page', () => {
     const {getByText} =  render(
       <Router>
         <PlayerCard/>
@@ -13,4 +13,12 @@ import {BrowserRouter as Router} from 'react-router-dom';
     );
     getByText(/List Below/i);
     
+  })
+
+  test('button is present and working', () => {
+      const {getByText} = render(<Router><PlayerCard/></Router>)
+
+      const backButton = getByText(/players/i);
+
+      fireEvent.click(backButton);
   })
